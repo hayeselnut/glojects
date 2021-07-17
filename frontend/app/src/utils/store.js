189c121e4edc775f40e09b7c519e7c0b7e090e18
Hayes Choy <hayeselnut2127@gmail.com>
2021-07-18 05:02:15 +1000

    Updates

--- frontend/app/src/utils/store.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from 'react';
-
-export const StoreContext = createContext(null);
-export const StoreProvider = ({ children }) => {
-  const store = {};
-  return (
-    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
-  );
-};
