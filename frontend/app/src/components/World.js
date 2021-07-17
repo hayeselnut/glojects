45cc629760035103b96e4e233cde3bf9836ed8a9
James Ji <jamesx.ji@gmail.com>
2021-07-17 23:53:25 +1000

    modal frontpage

--- frontend/app/src/components/World.js
+++ frontend/app/src/components/World.js
@@ -25,41 +25,74 @@ sampleData.forEach((marker, index) => {
 
 console.log(sampleData);
 
+const initOptions = {
+    enableMarkerGlow: true,
+    // markerGlowCoefficient: 1,
+    // markerGlowPower: 1,
+    // markerGlowRadiusScale: 0.8,
+    markerRadiusScaleRange: [0.005, 0.02],
+    markerType: 'dot',
+    enableMarkerToolTip: true,
+    // markerEnterAnimationDuration: 3000,
+    // markerEnterEasingFunction: ['Bounce', 'InOut'],
+    // markerExitEasingFunction: ['Cubic', 'Out'],
+    // markerTooltipRenderer: marker => 
+    //     `${marker.city} (Sales: ${marker.value}.0M)`,
+    // markerRadiusScaleRange: [0.01, 0.05],
+
+    cameraAutoRotateSpeed: 0.5,
+
+    focusAnimationDuration: 2000,
+    focusDistanceRadiusScale: 1.5,
+    cameraRotateSpeed: 0.5,
+    focusEasingFunction: ['Linear', 'None'],
+    enableDefocus: true,
+}
+
 const World = () => {
     const [projects, setProjects] = useState([]);
     const [focus, setFocus] = useState(null);
+    const [open, setOpen] = useState(true);
+    const [options, setOptions] = useState(initOptions);
+
+    const initZoom = () => {
+        const newOptions = {...options};
+        newOptions.cameraAutoRotateSpeed = 0.1;
+        setFocus([-33, 151]);
+        setOpen(false);
+        setOptions(newOptions);
+    }
+
     useEffect(() => {
         setProjects(sampleData);
-    }, [])
 
-    const options = {
-        enableMarkerGlow: true,
-        // markerGlowCoefficient: 1,
-        // markerGlowPower: 1,
-        // markerGlowRadiusScale: 0.8,
-        markerRadiusScaleRange: [0.005, 0.02],
-        markerType: 'dot',
-        enableMarkerToolTip: true,
-        // markerEnterAnimationDuration: 3000,
-        // markerEnterEasingFunction: ['Bounce', 'InOut'],
-        // markerExitEasingFunction: ['Cubic', 'Out'],
-        // markerTooltipRenderer: marker => 
-        //     `${marker.city} (Sales: ${marker.value}.0M)`,
-        // markerRadiusScaleRange: [0.01, 0.05],
-
-        // focusAnimationDuration: 2000,
-        // focusDistanceRadiusScale: 1.5,
-        cameraRotateSpeed: 0.5,
-        focusEasingFunction: ['Linear', 'None'],
-        enableDefocus: true,
-    }
+        window.addEventListener('keydown', () => {
+            initZoom();
+            window.removeEventListener('keydown', initZoom);
+        })
+    }, []);
 
     // const texture = 'https://raw.githubusercontent.com/chrisrzhou/react-globe/main/textures/globe_dark.jpg'
 
     return (
         <>  
-            <button onClick={() => setProjects(filterByExactField(projects, "experience", "beginner"))}>Filter by Beginner</button>
-            <button onClick={() => setFocus([1.3521, 103.8198])}>Singapore</button>
+            {/* <button onClick={() => setProjects(filterByExactField(projects, "experience", "beginner"))}>Filter by Beginner</button> */}
+            {/* <button onClick={() => setFocus([1.3521, 103.8198])}>Singapore</button> */}
+            { open ? <div style={startModalStyle}>
+                <div style={upperText}/>
+                <div style={middleText}>
+                    <div style={left}>
+                        G L
+                    </div>
+                    <div style={middle}/>
+                    <div style={right}>
+                        J E C T S
+                    </div>
+                </div>
+                <div style={lowerText}>
+                    Press any key to continue
+                </div>
+            </div> : null}
             <ReactGlobe 
                 globeTexture={texture}
                 focus={focus}
@@ -68,10 +101,73 @@ const World = () => {
                 markers={projects}
                 options={options}
                 onClickMarker={obj => zoomToMarker(setFocus, obj)}
-                initialCameraDistanceRadiusScale={40}
+                initialCameraDistanceRadiusScale={30}
             />
         </>
     )
 }
 
 export default World;
+
+const startModalStyle = {
+    width: "100%",
+    height: "100%",
+    position: "fixed",
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    // border: "10px solid blue",
+    display: "flex",
+    flexDirection: "column",
+    color: "white",
+    justifyContent: "center",
+
+    pointEvents: "none"
+}
+
+const upperText = {
+    flex: 4,
+    // border: "5px solid red",
+    // backgroundColor: "red",
+    // fontSize: "50px"
+}
+
+const middleText = {
+    flex: 3,
+    // backgroundColor: "green"
+    // border: "5px solid green",
+    fontSize: "8vw",
+    fontWeight: "bold",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    // letterSpacing: "1vw",
+    // marginLeft: "20vw",
+    // paddingLeft: "18%"
+}
+
+const left = {
+    flex: 4,
+    display: "flex",
+    flexDirection: "row",
+    // border: "2px solid purple",
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+}
+const middle = {
+    flex: 1,
+    // border: "2px solid white",
+}
+
+const right = {
+    flex: 4,
+    // border: "2px solid orange",
+    alignItems: "flex-start"
+}
+
+const lowerText = {
+    flex: 4,
+    // border: "5px solid yellow",
+    textAlign: "center"
+    // backgroundColor: "yellow"
+}
+
