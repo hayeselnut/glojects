fb20637779afb898326a2be6be5eaa3528fadbbe
Hayes Choy <hayeselnut2127@gmail.com>
2021-07-17 17:55:48 +1000

    fixed firebase config

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -6,19 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDnFzcl4K2gtIBIETg77m3a6N_izwZAGS4',
-  authDomain: 'glojects.firebaseapp.com',
-  projectId: 'glojects',
-  storageBucket: 'glojects.appspot.com',
-  messagingSenderId: '837750996466',
-  appId: '1:837750996466:web:79dc5e0db7ade834fb3f6b',
-  measurementId: 'G-6V45WZLM9W',
-};
-
-firebase.initializeApp(firebaseConfig);
+import { firebaseConfig } from './firebase/firebaseConfig';
 
 ReactDOM.render(
   <React.StrictMode>
