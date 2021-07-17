00bf82b3dd28b3deada3ddf693d015280828cd8d
Hayes Choy <hayeselnut2127@gmail.com>
2021-07-17 17:56:05 +1000

    fixed firebase config

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import { firebaseConfig } from './firebase/firebaseConfig';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
