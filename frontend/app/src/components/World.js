82ab9c0cf0d12c28dbdc71fa96dc7ada673758e0
James Ji <jamesx.ji@gmail.com>
2021-07-17 17:32:01 +1000

    world

--- frontend/app/src/components/World.js
+++ frontend/app/src/components/World.js
@@ -5,8 +5,8 @@ const sampleData = [...Array(100).keys()].map(() => ({
     projectName: "My Project",
     lat: (Math.random() - 0.5) * 180,
     lng: (Math.random() - 0.5) * 360,
-    size: 100,
-    color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+    size: 1,
+    color: ['blue', 'yellow', 'green', 'red'][Math.round(Math.random() * 3)]
 }));
 
 const World = () => {
@@ -16,17 +16,27 @@ const World = () => {
         setProjects(sampleData);
     }, []);
 
+    const filterByColor = (color) => {
+        const newProjects = projects.filter(p => p.color === color);
+        setProjects(newProjects);
+    }
+    
     return (
-        <Globe
-            globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-            backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-            pointsData={projects}
-            pointAltitude={0}
-            pointRadius={d => d.size}
-            pointLabel={d => d.projectName}
-            onPointClick={d => console.log("Clicked on ", d)}
-            onPointHover={d => console.log("Hovering on ", d)}
-        />
+        <>
+            <button onClick={() => {filterByColor("green")}}>FILTER</button>
+            <Globe
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+                backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+                pointsData={projects}
+                pointAltitude={0}
+                pointRadius={d => d.size}
+                pointLabel={d => d.projectName}
+                pointColor={d => d.color}
+                onPointClick={d => console.log("Clicked on ", d)}
+                onPointHover={d => console.log("Hovering on ", d)}
+                animateIn={true}
+            />
+        </>
     )
 }
 
