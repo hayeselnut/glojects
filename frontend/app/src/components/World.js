2e3ee333500b13c02421d80bd18754cf0f57b1e1
unknown <leolliu1@gmail.com>
2021-07-18 03:02:27 +1000

    added card - not showing properly

--- frontend/app/src/components/World.js
+++ frontend/app/src/components/World.js
@@ -5,11 +5,13 @@ import texture from './8k.jpeg';
 import { filterByExactField } from './WorldUtil/projectsUtil';
 import { zoomToMarker } from './WorldUtil/cameraAnimations';
 
+import GlobjectCard from '../components/common/GlojectCard';
+
 const sampleData = [...Array(25).keys()].map(() => ({
-    projectName: "Project Name",
-    experience: ["beginner", "moderate", "expert"][Math.round(Math.random() * 2)],
-    coordinates: [(Math.random() - 0.5) * 180, (Math.random() - 0.5) * 360],
-    value: 25,
+  projectName: 'Project Name',
+  experience: ['beginner', 'moderate', 'expert'][Math.round(Math.random() * 2)],
+  coordinates: [(Math.random() - 0.5) * 180, (Math.random() - 0.5) * 360],
+  value: 25,
 }));
 
 sampleData.forEach((marker, index) => {
@@ -26,160 +28,164 @@ sampleData.forEach((marker, index) => {
 console.log(sampleData);
 
 const initOptions = {
-    enableMarkerGlow: true,
-    // markerGlowCoefficient: 1,
-    // markerGlowPower: 1,
-    // markerGlowRadiusScale: 0.8,
-    markerRadiusScaleRange: [0.005, 0.02],
-    markerType: 'dot',
-    enableMarkerToolTip: true,
-    // markerEnterAnimationDuration: 3000,
-    // markerEnterEasingFunction: ['Bounce', 'InOut'],
-    // markerExitEasingFunction: ['Cubic', 'Out'],
-    // markerTooltipRenderer: marker => 
-    //     `${marker.city} (Sales: ${marker.value}.0M)`,
-    // markerRadiusScaleRange: [0.01, 0.05],
+  enableMarkerGlow: true,
+  // markerGlowCoefficient: 1,
+  // markerGlowPower: 1,
+  // markerGlowRadiusScale: 0.8,
+  markerRadiusScaleRange: [0.005, 0.02],
+  markerType: 'dot',
+  enableMarkerToolTip: true,
+  // markerEnterAnimationDuration: 3000,
+  // markerEnterEasingFunction: ['Bounce', 'InOut'],
+  // markerExitEasingFunction: ['Cubic', 'Out'],
+  // markerTooltipRenderer: marker =>
+  //     `${marker.city} (Sales: ${marker.value}.0M)`,
+  // markerRadiusScaleRange: [0.01, 0.05],
 
-    // cameraAutoRotateSpeed: 0.5,
+  // cameraAutoRotateSpeed: 0.5,
 
-    focusAnimationDuration: 2000,
-    focusDistanceRadiusScale: 1.5,
-    cameraRotateSpeed: 0.5,
-    focusEasingFunction: ['Linear', 'None'],
-    enableDefocus: true,
-}
+  focusAnimationDuration: 2000,
+  focusDistanceRadiusScale: 1.5,
+  cameraRotateSpeed: 0.5,
+  focusEasingFunction: ['Linear', 'None'],
+  enableDefocus: true,
+};
 
 const World = () => {
-    const [projects, setProjects] = useState([]);
-    const [focus, setFocus] = useState(null);
-    const [open, setOpen] = useState(true);
-    const [options, setOptions] = useState(initOptions);
+  const [projects, setProjects] = useState([]);
+  const [focus, setFocus] = useState(null);
+  const [open, setOpen] = useState(true);
+  const [options, setOptions] = useState(initOptions);
 
-    const initZoom = () => {
-        // const newOptions = {...options};
-        // newOptions.cameraAutoRotateSpeed = 0.1;
-        setFocus([-33, 151]);
-        setOpen(false);
-        // setOptions(newOptions);
-    }
+  const initZoom = () => {
+    // const newOptions = {...options};
+    // newOptions.cameraAutoRotateSpeed = 0.1;
+    setFocus([-33, 151]);
+    setOpen(false);
+    // setOptions(newOptions);
+  };
 
-    useEffect(() => {
-        setProjects(sampleData);
+  useEffect(() => {
+    setProjects(sampleData);
 
-        window.addEventListener('keydown', () => {
-            initZoom();
-            window.removeEventListener('keydown', initZoom);
-        })
-    }, []);
+    window.addEventListener('keydown', () => {
+      initZoom();
+      window.removeEventListener('keydown', initZoom);
+    });
+  }, []);
 
-    const onClick = (obj) => {
-        zoomToMarker(setFocus, obj);
-        const newOptions = {...options};
-        newOptions.cameraAutoRotateSpeed = 0;
-        setOptions(newOptions);
-    };
+  const onClick = (obj) => {
+    console.log('obj');
+    zoomToMarker(setFocus, obj);
+    const newOptions = { ...options };
+    newOptions.cameraAutoRotateSpeed = 0;
+    setOptions(newOptions);
+  };
 
-    const onDefocus = () => {
-        const newOptions = {...options};
-        newOptions.cameraAutoRotateSpeed = 0.1;
-        setOptions(newOptions);
-    }
+  const onDefocus = () => {
+    const newOptions = { ...options };
+    newOptions.cameraAutoRotateSpeed = 0.1;
+    setOptions(newOptions);
+  };
 
-    return (
-        <>  
-            {/* <button onClick={() => setProjects(filterByExactField(projects, "experience", "beginner"))}>Filter by Beginner</button> */}
-            {/* <button onClick={() => setFocus([1.3521, 103.8198])}>Singapore</button> */}
-            { open ? <div style={startModalStyle}>
-                <div style={upperText}/>
-                <div style={middleText}>
-                    <div style={left}>
-                        G L
-                    </div>
-                    <div style={middle}/>
-                    <div style={right}>
-                        J E C T S
-                    </div>
-                </div>
-                <div style={lowerText}>
-                    Press any key to continue
-                </div>
-            </div> : null}
-            <ReactGlobe 
-                globeTexture={texture}
-                focus={focus}
-                height="100vh"
-                width="100wh"
-                markers={projects}
-                options={options}
-                onClickMarker={onClick}
-                onDefocus={onDefocus}
-                initialCameraDistanceRadiusScale={25}
-            />
-        </>
-    )
-}
+  return (
+    <>
+      {/* <button onClick={() => setProjects(filterByExactField(projects, "experience", "beginner"))}>Filter by Beginner</button> */}
+      {/* <button onClick={() => setFocus([1.3521, 103.8198])}>Singapore</button> */}
+      {open ? (
+        <div style={startModalStyle}>
+          <div style={upperText} />
+          <div style={middleText}>
+            <div style={left}>G L</div>
+            <div style={middle} />
+            <div style={right}>J E C T S</div>
+          </div>
+          <div style={lowerText}>Press any key to continue</div>
+        </div>
+      ) : null}
+      <GlobjectCard
+        style={{ position: 'fixed', right: '200px' }}
+        src="https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg"
+        title="Boss Coffee Boss Iced Long Black"
+        tags={['coffee', 'tea']}
+        description="iced long black flas k brew breed hot / chilled fast - no1 coffee in japan kawaiiiiiidesu"
+        owner="35Z6uU2PpFRb9XbVG0rF"
+      />
+      <ReactGlobe
+        globeTexture={texture}
+        focus={focus}
+        height="100vh"
+        width="100wh"
+        markers={projects}
+        options={options}
+        onClickMarker={onClick}
+        onDefocus={onDefocus}
+        initialCameraDistanceRadiusScale={25}
+      />
+    </>
+  );
+};
 
 export default World;
 
 const startModalStyle = {
-    width: "100%",
-    height: "100%",
-    position: "fixed",
-    backgroundColor: 'rgba(0, 0, 0, 0)',
-    // border: "10px solid blue",
-    display: "flex",
-    flexDirection: "column",
-    color: "white",
-    justifyContent: "center",
+  width: '100%',
+  height: '100%',
+  position: 'fixed',
+  backgroundColor: 'rgba(0, 0, 0, 0)',
+  // border: "10px solid blue",
+  display: 'flex',
+  flexDirection: 'column',
+  color: 'white',
+  justifyContent: 'center',
 
-    pointEvents: "none"
-}
+  pointEvents: 'none',
+};
 
 const upperText = {
-    flex: 4,
-    // border: "5px solid red",
-    // backgroundColor: "red",
-    // fontSize: "50px"
-}
+  flex: 4,
+  // border: "5px solid red",
+  // backgroundColor: "red",
+  // fontSize: "50px"
+};
 
 const middleText = {
-    flex: 3,
-    // backgroundColor: "green"
-    // border: "5px solid green",
-    fontSize: "8vw",
-    fontWeight: "bold",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    // letterSpacing: "1vw",
-    // marginLeft: "20vw",
-    // paddingLeft: "18%"
-}
+  flex: 3,
+  // backgroundColor: "green"
+  // border: "5px solid green",
+  fontSize: '8vw',
+  fontWeight: 'bold',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  // letterSpacing: "1vw",
+  // marginLeft: "20vw",
+  // paddingLeft: "18%"
+};
 
 const left = {
-    flex: 4,
-    display: "flex",
-    flexDirection: "row",
-    // border: "2px solid purple",
-    alignItems: "flex-end",
-    justifyContent: "flex-end",
-}
+  flex: 4,
+  display: 'flex',
+  flexDirection: 'row',
+  // border: "2px solid purple",
+  alignItems: 'flex-end',
+  justifyContent: 'flex-end',
+};
 const middle = {
-    flex: 1,
-    // border: "2px solid white",
-}
+  flex: 1,
+  // border: "2px solid white",
+};
 
 const right = {
-    flex: 4,
-    // border: "2px solid orange",
-    alignItems: "flex-start"
-}
+  flex: 4,
+  // border: "2px solid orange",
+  alignItems: 'flex-start',
+};
 
 const lowerText = {
-    flex: 4,
-    // border: "5px solid yellow",
-    textAlign: "center"
-    // backgroundColor: "yellow"
-}
-
+  flex: 4,
+  // border: "5px solid yellow",
+  textAlign: 'center',
+  // backgroundColor: "yellow"
+};
