83ae518fd6c45712b3b13cf0fafb0204cf1da396
unknown <leolliu1@gmail.com>
2021-07-17 18:01:18 +1000

    merge conflicts

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
